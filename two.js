1a65462215a401c1197f697efdbd0f4884559a1a
Ahmeda Farhin <124598487+aFarhin@users.noreply.github.com>
2023-03-24 13:12:24 +0530

    Delete two.js

--- two.js
+++
@@ -1,9 +0,0 @@
-
-        // 1. at first i will show how to change only one property of an element
-        document.getElementById("heading1").style.color = "red";
-
-
-
-        // 2. now I will show how to change more than one properties in an element
-        document.getElementById("heading2").setAttribute("style",
-            "color:blue; background-color:pink;text-align: center; border-radius: 50%; border: 2px solid black");
